98dcc05996f25fcdd517437609c5b51feb20a0f6
Dibya Prakash Das <dibyadas998@gmail.com>
2020-06-26 00:38:04 +0530

    refactor

--- streams.py
+++ streams.py
@@ -19,23 +19,3 @@ async def tap_detector(reader):  # filter tap events
     async for event in reader:
         if event.type == ecodes.EV_KEY and event.code == 330:
             yield event
-
-# async def x_movement(device_path):  # filter events in X axis
-#     dev = evdev.InputDevice(device_path)
-#     async for event in dev.async_read_loop():
-#         if event.type == ecodes.EV_ABS and event.code == 0:
-#             yield event
-
-
-# async def y_movement(device_path):  # filter events in Y axis
-#     dev = evdev.InputDevice(device_path)
-#     async for event in dev.async_read_loop():
-#         if event.type == ecodes.EV_ABS and event.code == 1:
-#             yield event
-
-
-# async def tap_detector(device_path):  # filter tap events
-#     dev = evdev.InputDevice(device_path)
-#     async for event in dev.async_read_loop():
-#         if event.type == ecodes.EV_KEY and event.code == 330:
-#             yield event
